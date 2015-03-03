0d7f6f041e83d256317f0aa34440af6bae1b5e3d
Theodore Tripp <tjtripp@gmail.com>
2015-03-03 17:03:13 +0000

    Add exercise: Handling POST Requests with Sinatra.

--- exercises/handling-post-requests/app.rb
+++ exercises/handling-post-requests/app.rb
@@ -6,14 +6,46 @@ get '/' do
   erb :recipes
 end
 
+post '/recipes/:recipe_id' do
+  recipe = Recipe.get(params[:recipe_id])
+
+  recipe.update(:created_by => params[:created_by],
+                :title => params[:title],
+                :description => params[:description],
+                :instructions => params[:instructions],
+                :created_at => Time.now)
+  if recipe.saved?
+    redirect '/'
+  else
+    return "ERROR RECIPE NOT SAVED"
+  end
+end
+
+post '/recipes' do
+  new_recipe = Recipe.create(:created_by => params[:created_by],
+                             :title => params[:title],
+                             :description => params[:description],
+                             :instructions => params[:instructions],
+                             :created_at => Time.now)
+  if new_recipe.saved?
+    redirect '/'
+  else
+    return "ERROR RECIPE NOT SAVED"
+  end
+end
+
 get '/recipes/:recipe_id' do
   if @recipe = Recipe.get(params[:recipe_id])
     erb :show_recipe
   else
     404
-    # WHAT IS THIS MADNESS?!
-    # https://en.wikipedia.org/wiki/404_Error
-    # http://www.sinatrarb.com/intro.html#Return%20Values
   end
 end
 
+get '/recipes/:recipe_id/edit' do
+  if @recipe = Recipe.get(params[:recipe_id])
+    erb :edit_recipe
+  else
+    404
+  end
+end
