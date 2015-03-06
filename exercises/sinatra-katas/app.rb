c186ad1f29044fe0cfcd5fd1dff7cc0d6aacefa5
Theodore Tripp <tjtripp@gmail.com>
2015-03-06 03:47:37 +0000

    Add code to work with database and render views.

--- exercises/sinatra-katas/app.rb
+++ exercises/sinatra-katas/app.rb
@@ -23,57 +23,65 @@ DataMapper.auto_upgrade!
 # Update    | PUT
 # Destroy   | DELETE
 
-#CREATE 
+#CREATE
 
 # This will render an empty form so the user can enter
 # the information to create a new tweet
 get("/tweets/new") do
-  # build a 'shell' Tweet object that can be sent to the browser
-  # render the :new template
+  @tweet = Tweet.new # build a 'shell' Tweet object that can be sent to the browser
+  erb :new # render the :new template
 end
 
 # CRUD Verb: Create
 # HTTP Method: POST
- 
+
 # This is the endpoint our "create form" submits to using
 # the HTTP POST method.
 post("/tweets") do
-  # create a new tweet object and populate it with data from params hash
-  # redirect the user to "/" or render :new if the record didn't save
+  @tweet = Tweet.create(params[:tweet]) # create a new tweet object and populate it with data from params hash
+  if @tweet.saved? # redirect the user to "/" or render :new if the record didn't save
+    redirect "/"
+  else
+    erb :new
+  end
 end
 
 # READ
 
 get("/") do
-  # query the db for all tweets
-  # render the index template
+  @tweets = Tweet.all # query the db for all tweets
+  erb :index # render the index template
 end
 
 get("/tweets/:id") do
-  # query the db for the id of the requested tweet
-  # render the relevant template
+  @tweet = Tweet.get(params[:id]) # query the db for the id of the requested tweet
+  erb :show # render the relevant template
 end
 
-# UPDATE 
+# UPDATE
 
 # This renders the form used to edit a specific tweet
-get("/tweets/:id/edit") do
-  # query the db for the requested tweet
-  # render the edit template
+get("/tweets/edit/:id") do
+  @tweet = Tweet.get(params[:id]) # query the db for the requested tweet
+  erb :edit # render the edit template
 end
 
 # This is the endpoint our "edit form" submits to using
 # the HTTP PUT method.
 put("/tweets/:id") do
-  # query the db for the requested tweet
-  # update the tweet's attributes in the db
-  # redirect the user to "/" or render edit if the record didn't save
+  @tweet = Tweet.get(params[:id]) # query the db for the requested tweet
+  @tweet.update(params[:tweet]) # update the tweet's attributes in the db
+  if @tweet.saved?
+    redirect "/"
+    else
+      erb :edit
+    end # redirect the user to "/" or render edit if the record didn't save
 end
 
 # DESTROY
 
 delete("/tweets/:id") do
-  # query the db for the rquested tweet
-  # destroy the tweet
-  # redirect the user back to the root, "/"
+  @tweet = Tweet.get(params[:id]) # query the db for the rquested tweet
+  @tweet.destroy # destroy the tweet
+  redirect "/" # redirect the user back to the root, "/"
 end
